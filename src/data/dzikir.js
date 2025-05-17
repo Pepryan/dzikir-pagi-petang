9a91f3ee806df989b153497062f4db842fb22a18
Febryan Ramadhan <febryanramadhan@gmail.com>
2025-05-17 10:09:14 +0700

    feat(dzikir): add new dzikir entries for morning and evening
    This commit introduces additional dzikir entries for both morning and evening prayers, enhancing the collection of supplications available in the application. The new entries include detailed Arabic text, Latin transliteration, translation, and sources, ensuring users have a comprehensive set of dzikir to incorporate into their daily routines.

--- src/data/dzikir.js
+++ src/data/dzikir.js
@@ -82,11 +82,123 @@ const dzikirData = {
         },
         {
             id: 9,
-            title: "Dzikir Petang #1",
+            title: "Dzikir Pagi #3",
             arabic: "اَللَّهُمَّ عَافِنِيْ فِيْ بَدَنِيْ، اَللَّهُمَّ عَافِنِيْ فِيْ سَمْعِيْ، اَللَّهُمَّ عَافِنِيْ فِيْ بَصَرِيْ، لاَ إِلَـهَ إِلاَّ أَنْتَ. اَللَّهُمَّ إِنِّي أَعُوْذُ بِكَ مِنَ الْكُفْرِ وَالْفَقْرِ، وَأَعُوْذُ بِكَ مِنْ عَذَابِ الْقَبْرِ، لاَ إِلَـهَ إِلاَّ أَنْتَ",
             latin: "",
             translation: "Ya Allah, selamatkanlah tubuhku (dari penyakit dan dari apa yang tidak aku inginkan). Ya Allah, selamatkanlah pendengaranku (dari penyakit dan maksiat atau dari apa yang tidak aku inginkan). Ya Allah, selamatkanlah penglihatanku, tidak ada Ilah yang berhak diibadahi dengan benar kecuali Engkau. Ya Allah, sesungguhnya aku berlindung kepada-Mu dari kekufuran dan kefakiran. Aku berlindung kepada-Mu dari siksa kubur, tidak ada Ilah yang berhak diibadahi dengan benar kecuali Engkau.",
             source: "HR. Al-Bukhari dalam Shahiib al-Adabil Mufrad no. 701, Abu Dawud no. 5090, Ahmad V/42, hasan. Lihat Shahiih Al-Adabil Mufrad no.539",
+            count: 3,
+            counter: 0
+        },
+        {
+            id: 10,
+            title: "Dzikir Pagi #4",
+            arabic:"اَللَّهُمَّ إِنِّيْ أَسْأَلُكَ الْعَفْوَ وَالْعَافِيَةَ فِي الدُّنْيَا وَاْلآخِرَةِ، اَللَّهُمَّ إِنِّيْ أَسْأَلُكَ الْعَفْوَ وَالْعَافِيَةَ فِي دِيْنِيْ وَدُنْيَايَ وَأَهْلِيْ وَمَالِيْ اللَّهُمَّ اسْتُرْ عَوْرَاتِى وَآمِنْ رَوْعَاتِى. اَللَّهُمَّ احْفَظْنِيْ مِنْ بَيْنِ يَدَيَّ، وَمِنْ خَلْفِيْ، وَعَنْ يَمِيْنِيْ وَعَنْ شِمَالِيْ، وَمِنْ فَوْقِيْ، وَأَعُوْذُ بِعَظَمَتِكَ أَنْ أُغْتَالَ مِنْ تَحْتِيْ",
+            latin: "Allahumma innii as-alukal 'afwa wal 'aafiyah fid dunyaa wal aakhiroh. Allahumma innii as-alukal 'afwa wal 'aafiyah fii diinii wa dun-yaaya wa ahlii wa maalii. Allahumas-tur 'awrootii wa aamin row'aatii. Allahummahfazh-nii mim bayni yadayya wa min kholfii wa 'an yamiinii wa 'an syimaalii wa min fawqii wa a'udzu bi 'azhomatik an ughtaala min tahtii.",
+            translation: "Ya Allah, sesungguhnya aku memohon kebajikan dan keselamatan di dunia dan akhirat. Ya Allah, sesungguhnya aku memohon kebajikan dan keselamatan dalam agama, dunia, keluarga dan hartaku. Ya Allah, tutupilah auratku (aib dan sesuatu yang tidak layak dilihat orang) dan tenteramkanlah aku dari rasa takut. Ya Allah, peliharalah aku dari muka, belakang, kanan, kiri dan atasku. Aku berlindung dengan kebesaran-Mu, agar aku tidak disambar dari bawahku (oleh ular atau tenggelam dalam bumi dan lain-lain yang membuat aku jatuh).",
+            source: "Faedah: Rasulullah shallallahu 'alaihi wa sallam tidaklah pernah meninggalkan do'a ini di pagi dan petang hari. Di dalamnya berisi perlindungan dan keselamatan pada agama, dunia, keluarga dan harta dari berbagai macam gangguan yang datang dari berbagai arah. Hikmah: Dzikir ini mengajarkan kita untuk memohon perlindungan Allah dari segala arah dan aspek kehidupan. Ini mengingatkan kita bahwa kita membutuhkan perlindungan Allah dalam setiap saat dan dari segala arah. Sumber: HR. Abu Daud no. 5074 dan Ibnu Majah no. 3871",
+            count: 1,
+            counter: 0
+        },
+        {
+            id: 11,
+            title: "Dzikir Pagi #5",
+            arabic: "اَللَّهُمَّ عَالِمَ الْغَيْبِ وَالشَّهَادَةِ فَاطِرَ السَّمَاوَاتِ وَاْلأَرْضِ، رَبَّ كُلِّ شَيْءٍ وَمَلِيْكَهُ، أَشْهَدُ أَنْ لاَ إِلَـهَ إِلاَّ أَنْتَ، أَعُوْذُ بِكَ مِنْ شَرِّ نَفْسِيْ، وَمِنْ شَرِّ الشَّيْطَانِ وَشِرْكِهِ، وَأَنْ أَقْتَرِفَ عَلَى نَفْسِيْ سُوْءًا أَوْ أَجُرَّهُ إِلَى مُسْلِمٍ",
+            latin: "Allahumma 'aalimal ghoybi wasy syahaadah faathiros samaawaati wal ardh. Robba kulli syai-in wa maliikah. Asyhadu alla ilaha illa anta. A'udzu bika min syarri nafsii wa min syarrisy syaythooni wa syirkihi, wa an aqtarifa 'alaa nafsii suu-an aw ajurrohu ilaa muslim.",
+            translation: "Ya Allah, Yang Maha Mengetahui yang ghaib dan yang nyata, wahai Rabb pencipta langit dan bumi, Rabb segala sesuatu dan yang merajainya. Aku bersaksi bahwa tidak ada ilah yang berhak disembah kecuali Engkau. Aku berlindung kepadaMu dari kejahatan diriku, setan dan balatentaranya (godaan untuk berbuat syirik pada Allah), dan aku (berlindung kepada-Mu) dari berbuat kejelekan terhadap diriku atau menyeretnya kepada seorang muslim.",
+            source:"",
+            count:1,
+            counter:0
+        },
+        {
+            id: 12,
+            title: "Dzikir Pagi #6",
+            arabic:"بِسْمِ اللَّهِ الَّذِي لاَ يَضُرُّ مَعَ اسْمِهِ شَيْءٌ فِي اْلأَرْضِ وَلاَ فِي السَّمَاءِ وَهُوَ السَّمِيْعُ الْعَلِيْمُ",
+            latin:"Bismillahilladzi laa yadhurru ma'asmihi syai-un fil ardhi wa laa fis samaa' wa huwas samii'ul 'aliim.",
+            translation:"Dengan nama Allah yang bila disebut, segala sesuatu di bumi dan langit tidak akan berbahaya, Dia-lah Yang Maha Mendengar lagi Maha Mengetahui.",
+            source:"Faedah: Barangsiapa yang mengucapkan dzikir tersebut sebanyak tiga kali di pagi hari dan tiga kali di petang hari, maka tidak akan ada bahaya yang tiba-tiba memudaratkannya. \n Hikmah: Dzikir ini mengajarkan kita untuk selalu menyebut nama Allah sebagai perlindungan dari segala bahaya. Ini mengingatkan kita bahwa dengan menyebut nama Allah, kita akan terlindungi dari bahaya di bumi dan di langit. Sumber:HR. Abu Daud no. 5088, 5089, Tirmidzi no. 3388, dan Ibnu Majah no. 3869.",
+            count:3,
+            counter:0
+        },
+        {
+            id: 13,
+            title: "Dzikir Pagi #7",
+            arabic:"رَضِيْتُ بِاللهِ رَبًّا، وَبِاْلإِسْلاَمِ دِيْنًا، وَبِمُحَمَّدٍ صَلَّى اللهُ عَلَيْهِ وَسَلَّمَ نَبِيًّا",
+            latin:"Rodhiitu billaahi robbaa, wa bil-islaami diinaa, wa bi-muhammadin shallallaahu 'alaihi wa sallama nabiyya.",
+            translation:"Aku ridha Allah sebagai Rabb, Islam sebagai agama dan Muhammad shallallahu 'alaihi wa sallam sebagai nabi.",
+            source:"Faedah: Barangsiapa yang mengucapkan dzikir ini sebanyak tiga kali di pagi hari dan tiga kali di petang hari, maka pantas baginya mendapatkan ridha Allah. Hikmah: Dzikir ini mengajarkan kita untuk meneguhkan keimanan kita kepada Allah, Islam, dan Nabi Muhammad SAW. Ini adalah bentuk pengakuan dan kerelaan hati terhadap tiga hal fundamental dalam keimanan seorang Muslim. Sumber: HR. Abu Daud no. 5072, Tirmidzi no. 3389",
+            count:3,
+            counter:0
+        },
+        {
+            id: 14,
+            title: "Dzikir Pagi #8",
+            arabic:"يَا حَيُّ يَا قَيُّوْمُ بِرَحْمَتِكَ أَسْتَغِيْثُ، أَصْلِحْ لِيْ شَأْنِيْ كُلَّهُ وَلاَ تَكِلْنِيْ إِلَى نَفْسِيْ طَرْفَةَ عَيْنٍ",
+            latin:"Yaa Hayyu Yaa Qoyyum, bi-rohmatika astaghiits, wa ash-lih lii sya'nii kullahu wa laa takilnii ilaa nafsii thorfata 'ain.",
+            translation:"Wahai Rabb Yang Maha Hidup, wahai Rabb Yang Berdiri Sendiri (tidak butuh segala sesuatu), dengan rahmat-Mu aku minta pertolongan, perbaikilah segala urusanku dan jangan diserahkan kepadaku sekali pun sekejap mata (tanpa mendapat pertolongan dari-Mu).",
+            source:"Faedah: Dzikir ini diajarkan oleh Nabi shallallahu 'alaihi wa sallam pada Fathimah supaya diamalkan pagi dan petang. Hikmah: Dzikir ini mengajarkan kita untuk selalu bergantung pada Allah dan memohon pertolongan-Nya dalam segala urusan. Ini mengingatkan kita bahwa kita tidak bisa mandiri bahkan sekejap mata pun tanpa pertolongan Allah. Sumber: HR. Ibnu As-Sunni dalam 'Amalul Yaum wal Lailah no. 46, An-Nasa'i dalam Al-Kubra (381/ 570), Al-Bazzar dalam musnadnya (4/ 25/ 3107)",
+            count:1,
+            counter:0
+        },
+        {
+            id: 15,
+            title: "Dzikir Pagi #9",
+            arabic:"أَصْبَحْنَا عَلَى فِطْرَةِ اْلإِسْلاَمِ وَعَلَى كَلِمَةِ اْلإِخْلاَصِ، وَعَلَى دِيْنِ نَبِيِّنَا مُحَمَّدٍ صَلَّى اللهُ عَلَيْهِ وَسَلَّمَ، وَعَلَى مِلَّةِ أَبِيْنَا إِبْرَاهِيْمَ، حَنِيْفًا مُسْلِمًا وَمَا كَانَ مِنَ الْمُشْرِكِيْنَ",
+            latin:"Ash-bahnaa 'ala fithrotil islaam wa 'alaa kalimatil ikhlaash, wa 'alaa diini nabiyyinaa Muhammadin shallallahu 'alaihi wa sallam, wa 'alaa millati abiinaa Ibraahiima haniifam muslimaaw wa maa kaana minal musyrikin.",
+            translation:"Di waktu pagi kami memegang agama Islam, kalimat ikhlas (kalimat syahadat), agama Nabi kami Muhammad shallallahu 'alaihi wa sallam, dan agama bapak kami Ibrahim, yang berdiri di atas jalan yang lurus, muslim dan tidak tergolong orang-orang musyrik.",
+            source:"Faedah: Dzikir ini menunjukkan bahwa kita berada di atas fitrah Islam, kalimat ikhlas (kalimat syahadat), agama Nabi Muhammad shallallahu 'alaihi wa sallam, dan millah (agama) Ibrahim yang lurus. Hikmah: Dzikir ini mengajarkan kita untuk meneguhkan identitas kita sebagai Muslim yang mengikuti ajaran Nabi Muhammad SAW dan agama Ibrahim yang lurus. Ini mengingatkan kita untuk selalu berpegang teguh pada fitrah Islam dan menjauhi kesyirikan. Sumber: HR. Ahmad (3/406-407), Ad-Darimi (2/292) dan lain-lain",
+            count:1,
+            counter:0
+        },
+        {
+            id: 16,
+            title: "Dzikir Pagi #10",
+            arabic:"سُبْحَانَ اللهِ وَبِحَمْدِهِ: عَدَدَ خَلْقِهِ، وَرِضَا نَفْسِهِ، وَزِنَةَ عَرْشِهِ وَمِدَادَ كَلِمَاتِهِ",
+            latin:"Subhanallah wa bihamdih, adada khalqih, wa ridha nafsihh, wa zinaa arshih wa midada kalimatih.",
+            translation:"Mahasuci Allah, aku memuji-Nya sebanyak bilangan makhluk-Nya, Mahasuci Allah sesuai ke-ridhaan-Nya, Mahasuci seberat timbangan ‘Arsy-Nya, dan Mahasuci sebanyak tinta (yang menulis) kalimat-Nya.",
+            source:"HR. Muslim no. 2726. Syarah Muslim XVII/44. Dari Juwairiyah binti al- Harits radhiyallahu ‘anhuma.",
+            count:3,
+            counter:0
+        },
+        {
+            id: 17,
+            title: "Dzikir Pagi #11",
+            arabic:"اَللَّهُمَّ إِنِّيْ أَسْأَلُكَ عِلْمًا نَافِعًا، وَرِزْقًا طَيِّبًا، وَعَمَلاً مُتَقَبَّلاً",
+            latin:"",
+            translation:"Ya Allah, sesungguhnya aku meminta kepada-Mu ilmu yang bermanfaat, rizki yang halal, dan amalan yang diterima.",
+            source:"HR. Ibnu Majah no. 925, Shahiih Ibni Majah 1/152 no. 753 Ibnus Sunni dalam ‘Amalul Yaum wal Lailah no. 54,110, dan Ahmad VI / 294, 305, 318, 322. Dari Ummu Salamah, shahih.",
+            count:1,
+            counter:0
+        },
+        {
+            id: 18,
+            title: "Dzikir Pagi #12",
+            arabic:"لاَ إِلَـهَ إِلاَّ اللهُ وَحْدَهُ لاَ شَرِيْكَ لَهُ، لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيْرُ",
+            latin:"Laa ilaha illa Allahu wa hadhu laa shariki lahu, lahu mulkuu wa lahu hamduu wa huwa 'alaa kulli syai-in qadiru.",
+            translation:"Tidak ada ilah yang berhak disembah selain Allah semata, tidak ada sekutu bagi-Nya. Milik Allah kerajaan dan segala pujian. Dia-lah yang berkuasa atas segala sesuatu.",
+            source:"Faedah: Barangsiapa yang mengucapkan dzikir tersebut di pagi hari sebanyak 10x, Allah akan mencatatkan baginya 10 kebaikan, menghapuskan baginya 10 kesalahan, ia juga mendapatkan kebaikan semisal memerdekakan 10 budak, Allah akan melindunginya dari gangguan setan hingga petang hari. Siapa yang mengucapkannya di petang hari, ia akan mendapatkan keutamaan semisal itu pula. Hikmah: Dzikir ini mengajarkan kita tentang tauhid (keesaan Allah) dan kekuasaan-Nya atas segala sesuatu. Ini mengingatkan kita bahwa hanya Allah yang berhak disembah dan Dia memiliki kekuasaan mutlak atas segala sesuatu. Sumber: HR. Ahmad (3/406-407), Ad-Darimi (2/292) dan lain-lain",
+            count:10,
+            counter:0
+        },
+        {
+            id: 19,
+            title: "Dzikir Pagi #13",
+            arabic:"سُبْحَانَ اللهِ وَبِحَمْدِهِ",
+            latin:"Subhanallahi wa bihamdihi.",
+            translation:"Maha Suci Allah, aku memuji-Nya.",
+            source:"Faedah: Barangsiapa yang mengucapkan kalimat 'Subhanallahi wa bihamdihi' di pagi dan petang hari sebanyak 100x, maka tidak ada yang datang pada hari kiamat yang lebih baik dari yang ia lakukan kecuali orang yang mengucapkan semisal atau lebih dari itu. Hikmah: Dzikir ini mengajarkan kita untuk selalu mensucikan dan memuji Allah. Ini adalah bentuk pengakuan akan kesempurnaan Allah dan rasa syukur atas segala nikmat-Nya. Sumber: HR. Muslim no. 2692",
+            count:100,
+            counter:0
+        },
+        {
+            id: 20,
+            title: "Dzikir Pagi #14",
+            arabic:"أَسْتَغْفِرُ اللهَ وَأَتُوْبُ إِلَيْهِ",
+            latin:"Astaghfirullah wa atubu illaih.",
+            translation:"Aku memohon ampunan kepada Allah dan bertaubat kepada-Nya.",
+            source:"HR. Al-Bukhari/ Fat-hul Baari XI/101 dan Muslim no.2702 عَنِ ابْنِ عُمَرَ قَالَ:قَالَ رَسُو لُ اللهِ صلي الله عليه وسلم : يَااَيُّهَا النَّسُ، تُوبُواإِلَيْ اللهِ. فَإِنِّيْ اَتُوبُ فِيْ الْيَومِ إِلَيْهِ مِانَةً مَرَّةٍ Dari Ibnu ‘Umar ia berkata: “Rasulullah صلي الله عليه وسلم bersabda: ‘Wahai manusia, bertaubatlah kalian kepada Allah, sesungguhnya aku bertaubat kepada-Nya dalam sehari seratus kali.’” [HR. Muslim no. 2702 (42)]. Dalam riwayat lain dari Agharr al-Muzani, Rasulullah صلي الله عليه وسلم bersabda: [إِنَّهُ لَيُغَانُ عَلَى قَلْبِيْ وَإِنِّيْ لأَسْتَغْفِرُ اللهَ فِي الْيَوْمِ مِائَةَ مَرَّةٍ] “Sesungguhnya hatiku terkadang lupa, dan sesungguhnya aku istighfar (minta ampun) kepada Allah dalam sehari seratus kali.” [HR. Muslim no. 2702 (41)] Nabi صلي الله عليه وسلم bersabda: “Barangsiapa yang mengucapkan: أَسْتَغْفِرُ اللهَ الْعَظِيْمَ الَّذِيْ لاَ إِلَـهَ إِلاَّ هُوَ الْحَيُّ الْقَيُّوْمُ وَأَتُوْبُ إِلَيْهِ ‘Aku memohon ampunan kepada Allah Yang Maha Agung, Yang tidak ada Ilah yang berhak diibadahi kecuali Dia, Yang Maha hidup lagi Maha berdiri sendiri dan aku bertaubat kepada-Nya.’ Maka Allah akan mengampuni dosanya meskipun ia pernah lari dari medan perang.” HR. Abu Dawud no. 1517, at-Tirmidzi no. 3577 dan al-Hakim I/511. Lihat Shahiih at-Tirmidzi III/282 no. 2381. Ayat yang menganjurkan istighfar dan taubat di antaranya: (QS. Huud: 3), (QS. An-Nuur: 31), (QS. At-Tahriim: 8) dan lain-lain.",
+            count:100,
+            counter:0
         }
     ],
     petang: [
@@ -164,7 +276,27 @@ const dzikirData = {
             source: "Dibaca di pagi hari. (HR. Tirmidzi dari Abu Hurairah)",
             count: 1,
             counter: 0
-        }
+        },
+        {
+            id: 9,
+            title: "Dzikir Petang #3",
+            arabic:"اَللَّهُمَّ إِنِّيْ أَسْأَلُكَ الْعَفْوَ وَالْعَافِيَةَ فِي الدُّنْيَا وَاْلآخِرَةِ، اَللَّهُمَّ إِنِّيْ أَسْأَلُكَ الْعَفْوَ وَالْعَافِيَةَ فِي دِيْنِيْ وَدُنْيَايَ وَأَهْلِيْ وَمَالِيْ اللَّهُمَّ اسْتُرْ عَوْرَاتِى وَآمِنْ رَوْعَاتِى. اَللَّهُمَّ احْفَظْنِيْ مِنْ بَيْنِ يَدَيَّ، وَمِنْ خَلْفِيْ، وَعَنْ يَمِيْنِيْ وَعَنْ شِمَالِيْ، وَمِنْ فَوْقِيْ، وَأَعُوْذُ بِعَظَمَتِكَ أَنْ أُغْتَالَ مِنْ تَحْتِيْ",
+            latin: "Allahumma innii as-alukal 'afwa wal 'aafiyah fid dunyaa wal aakhiroh. Allahumma innii as-alukal 'afwa wal 'aafiyah fii diinii wa dun-yaaya wa ahlii wa maalii. Allahumas-tur 'awrootii wa aamin row'aatii. Allahummahfazh-nii mim bayni yadayya wa min kholfii wa 'an yamiinii wa 'an syimaalii wa min fawqii wa a'udzu bi 'azhomatik an ughtaala min tahtii.",
+            translation: "Ya Allah, sesungguhnya aku memohon kebajikan dan keselamatan di dunia dan akhirat. Ya Allah, sesungguhnya aku memohon kebajikan dan keselamatan dalam agama, dunia, keluarga dan hartaku. Ya Allah, tutupilah auratku (aib dan sesuatu yang tidak layak dilihat orang) dan tenteramkanlah aku dari rasa takut. Ya Allah, peliharalah aku dari muka, belakang, kanan, kiri dan atasku. Aku berlindung dengan kebesaran-Mu, agar aku tidak disambar dari bawahku (oleh ular atau tenggelam dalam bumi dan lain-lain yang membuat aku jatuh).",
+            source: "Faedah: Rasulullah shallallahu 'alaihi wa sallam tidaklah pernah meninggalkan do'a ini di pagi dan petang hari. Di dalamnya berisi perlindungan dan keselamatan pada agama, dunia, keluarga dan harta dari berbagai macam gangguan yang datang dari berbagai arah. Hikmah: Dzikir ini mengajarkan kita untuk memohon perlindungan Allah dari segala arah dan aspek kehidupan. Ini mengingatkan kita bahwa kita membutuhkan perlindungan Allah dalam setiap saat dan dari segala arah. Sumber: HR. Abu Daud no. 5074 dan Ibnu Majah no. 3871",
+            count: 1,
+            counter: 0
+        },
+        {
+            id: 10,
+            title: "Dzikir Pagi #3",
+            arabic: "اَللَّهُمَّ عَافِنِيْ فِيْ بَدَنِيْ، اَللَّهُمَّ عَافِنِيْ فِيْ سَمْعِيْ، اَللَّهُمَّ عَافِنِيْ فِيْ بَصَرِيْ، لاَ إِلَـهَ إِلاَّ أَنْتَ. اَللَّهُمَّ إِنِّي أَعُوْذُ بِكَ مِنَ الْكُفْرِ وَالْفَقْرِ، وَأَعُوْذُ بِكَ مِنْ عَذَابِ الْقَبْرِ، لاَ إِلَـهَ إِلاَّ أَنْتَ",
+            latin: "",
+            translation: "Ya Allah, selamatkanlah tubuhku (dari penyakit dan dari apa yang tidak aku inginkan). Ya Allah, selamatkanlah pendengaranku (dari penyakit dan maksiat atau dari apa yang tidak aku inginkan). Ya Allah, selamatkanlah penglihatanku, tidak ada Ilah yang berhak diibadahi dengan benar kecuali Engkau. Ya Allah, sesungguhnya aku berlindung kepada-Mu dari kekufuran dan kefakiran. Aku berlindung kepada-Mu dari siksa kubur, tidak ada Ilah yang berhak diibadahi dengan benar kecuali Engkau.",
+            source: "HR. Al-Bukhari dalam Shahiib al-Adabil Mufrad no. 701, Abu Dawud no. 5090, Ahmad V/42, hasan. Lihat Shahiih Al-Adabil Mufrad no.539",
+            count: 3,
+            counter: 0
+        },
     ]
 };
 
